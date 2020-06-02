867d2cfa8f8ada7683139899b13cb7de86da37c2
Calvin Cheung <cheungcalvin2@gmail.com>
2020-06-02 00:39:36 -0400

    Added ability for bot to join and leave voice channels.

--- commands/music.js
+++ commands/music.js
@@ -1,11 +1,30 @@
 module.exports = {
     name: 'music',
     description: 'Allows user to request a song for the bot to play',
-    execute(message, args) {
+    async execute(message, args) {
         // code here
         if(!message.guild) return;
-        
-        message.reply('what do you want');
-        console.log("WAT");
+
+        const channel = message.member.voice.channel;
+        const bot = message.guild.me;
+
+        switch(args[0]) {
+            case 'j':
+                if(channel) {
+                    const connection = await channel.join();
+                } else {
+                    message.reply('You need to join a voice channel first!');
+                }
+                break;
+            case 'l':
+                if(bot.voice.channel != null) {
+                    bot.voice.channel.leave();
+                    message.channel.send('Successfully left the voice channel!');
+                } else {
+                    message.reply('I\'m not currently in a voice channel!');
+                }
+                // channel.leave();
+                break;
+        }
     }
 }
