ce8a74b987a5f34832d1885b4f7391d2d36c3816
Calvin Cheung <cheungcalvin2@gmail.com>
2021-07-12 11:25:39 -0400

    Adding two more aliases to the choose command.

--- commands/decisionMaker.js
+++ commands/decisionMaker.js
@@ -4,8 +4,8 @@ module.exports = {
     name: 'choose',
     description: `Input a bunch of items and I will select one of them for you!
                     e.g. \`!choose <item1 item2 item3...>\`
-                    *aliases*: \`choose\`, \`ch\``,
-    aliases: ['ch'],
+                    *aliases*: \`choose\`, \`ch\`, \`r\`, \`random\``,
+    aliases: ['ch', 'r', 'random'],
     execute(message, data) {
         if(data.args.length <= 1) {
             return message.reply(`Please provide at least 2 items!`);
